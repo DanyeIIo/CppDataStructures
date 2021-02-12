cb9ef33dc66ced9f69ddc75b90cd32f2a74f4ac4
DanyeIIo <ulkiora.panda@gmail.com>
2021-02-12 18:58:55 +0600

    stack

---
+++ 116 stack ht/Source.cpp
@@ -0,0 +1,72 @@
+﻿#include"Header.h"
+int main()
+{
+    system("color 0b");
+    Stack myStack(255);
+    myStack.Input();
+    myStack.Zadanie();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
